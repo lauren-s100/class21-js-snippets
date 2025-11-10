7283926cf9a62237cc9bd32987e3e904467373dd
lauren-s100 <les292@cornell.edu>
2025-11-10 16:17:43 +0000

    banner

--- scripts/cookie-banner.js
+++ scripts/cookie-banner.js
@@ -1,2 +1,10 @@
 // when #cookie-accept-button is clicked:
 //   add .hidden to #cookie-banner
+$("#cookie-accept-button").on("click", function() { // ← click event
+
+  // ↓ action snippets ↓
+  $("#cookie-banner").addClass("hidden");
+
+  // ↑ action snippets ↑
+
+}); // ← click event
--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,20 @@
-$(document).ready(function() { // ← page ready event
+$(document).ready(function () { // ← page ready event
 
-  // ↓ action snippets ↓
-  console.log("page ready");
-  // ↑ action snippets ↑
+    // ↓ action snippets ↓
+    if (window.matchMedia("(max-width: 500)").matches) { // ← get browser width
+
+        // ↓ action snippets (< width) ↓
+        console.log("page ready");
+        // ↑ action snippets (< width) ↑
+
+    } else { // ← get browser width
+
+        // ↓ action snippets (> width) ↓
+        console.log("page ready");
+        // ↑ action snippets (> width) ↑
+
+    } // ← get browser width
+
+    // ↑ action snippets ↑
 
 }); // ← page ready event
